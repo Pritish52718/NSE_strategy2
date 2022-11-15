97052655451cfa303c85a5edb65bd0895e200f39
Pritish Nayak <40473746+Pritish52718@users.noreply.github.com>
2022-11-15 10:45:29 +0530

    Update NSE_Strategy_code_v4.py

--- NSE_Strategy_code_v4.py
+++ NSE_Strategy_code_v4.py
@@ -110,8 +110,7 @@ logger.setLevel(logging.INFO)
 
 
 #Populating today's date as default, if the stat_date and/or End_date is not provided.
-#@st.experimental_memo
-@st.cache
+#@st.cache
 def downld_data():
     
     dfns=pd.DataFrame()
