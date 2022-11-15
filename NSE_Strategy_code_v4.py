53f33e72e019325e5751efdb48cb8aeadb9f3fda
Pritish Nayak <40473746+Pritish52718@users.noreply.github.com>
2022-11-15 12:32:55 +0530

    Update NSE_Strategy_code_v4.py

--- NSE_Strategy_code_v4.py
+++ NSE_Strategy_code_v4.py
@@ -110,7 +110,7 @@ logger.setLevel(logging.INFO)
 
 
 #Populating today's date as default, if the stat_date and/or End_date is not provided.
-@st.cache
+@st.cache(ttl=18000)
 def downld_data():
     
     dfns=pd.DataFrame()
@@ -257,7 +257,7 @@ st.sidebar.write('Your selected input type:', check_type)
 #     st.legacy_caching.clear_cache
 # #         st.experimental_memo.clear()
 # #         st. experimental_rerun()
-st.markdown("Data till: "+lis[-1].strftime("%d-%b-%Y"))
+#st.markdown("Data till: "+lis[-1].strftime("%d-%b-%Y"))
 if check_type=='NSE_stocks':
 #     if st.button('Download latest Data'):
 #         st.runtime.legacy_caching.clear_cache()
